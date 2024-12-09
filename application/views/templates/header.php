d0f0ff66418257dc4012671818719e6fbc3a8002
kymrs <muhamadrizkysaputra34@gmail.com>
2024-12-09 13:13:50 +0700

    tes

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -17,13 +17,13 @@
     <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Nunito:wght@600;700;800&family=Pacifico&display=swap" rel="stylesheet">
 
     <!-- Icon Font Stylesheet -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet" >
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
-    <link href="<?= base_url('assets/lib/animate/animate.min.css') ?>" rel="stylesheet" >
-    <link href="<?= base_url('assets/lib/owlcarousel/assets/owl.carousel.min.css') ?>" rel="stylesheet" >
-    <link href="<?= base_url('assets/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css') ?>" rel="stylesheet" >
+    <link href="<?= base_url('assets/lib/animate/animate.min.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/lib/owlcarousel/assets/owl.carousel.min.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css') ?>" rel="stylesheet">
 
     <!-- Customized Bootstrap Stylesheet -->
     <link href=" <?= base_url('assets/css/bootstrap.min.css') ?> " rel="stylesheet">
@@ -33,8 +33,8 @@
 </head>
 
 <body>
-    
-<div class="container-xxl bg-white p-0">
+
+    <div class="container-xxl bg-white p-0">
         <!-- Spinner Start -->
         <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
             <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
@@ -42,6 +42,3 @@
             </div>
         </div>
         <!-- Spinner End -->
-
-        
-     
